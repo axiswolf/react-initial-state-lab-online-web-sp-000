7816baabcd57ab6f42b2b4489a6fff5fd5a6b3be
Karen V <karenvo@usc.edu>
2020-01-16 14:01:17 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,5 +1,15 @@
 // your ImageSlider code here!
+import React from 'react';
+import Bomb from './Bomb'
+
 class ImageSlider extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    }
+  }
   render() {
     return (
 

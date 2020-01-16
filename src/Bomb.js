7fa80d0761bbd0b3298c1d2050a0510d15f6a2b4
Karen V <karenvo@usc.edu>
2020-01-16 14:00:31 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -2,6 +2,12 @@
 import React from 'react;'
 
 class Bomb extends React.Component {
+  constructor (props) {
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
   render() {
     return (
 
